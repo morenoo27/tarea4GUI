444f240a67e9b741895d35f8682f8ed2b1f8a35f
moreno27 <alejanmm2000@gmail.com>
2021-03-25 13:44:25 +0100

    formateado de texto

--- src/main/java/calculadoraVico/PanelBotones.java
+++ src/main/java/calculadoraVico/PanelBotones.java
@@ -9,25 +9,24 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-
 /**
- * 
+ *
  * @author alejandro
  */
 public class PanelBotones extends JPanel {
-    
+
     // Números del 0 al 9 y seis botones más para
     // suma, resta, multiplicacion, división, resultado y limpiar
-    JButton [] grupoBotones = new JButton[16];
-    
-    public PanelBotones(){
-            initComponents();
+    JButton[] grupoBotones = new JButton[16];
+
+    public PanelBotones() {
+        initComponents();
     }
-    
-    private void initComponents(){
-        
+
+    private void initComponents() {
+
         // Creación de los botones
-        for (int i=0;i<10;i++){
+        for (int i = 0; i < 10; i++) {
             grupoBotones[i] = new JButton(Integer.toString(i));
         }
         grupoBotones[10] = new JButton("+");
@@ -35,18 +34,17 @@ public class PanelBotones extends JPanel {
         grupoBotones[12] = new JButton("*");
         grupoBotones[13] = new JButton("/");
         grupoBotones[14] = new JButton("=");
-grupoBotones[15] = new JButton("C");
-       
-// Establecemos el layout
-        this.setLayout (new GridLayout (4,4));
+        grupoBotones[15] = new JButton("C");
+
+        // Establecemos el layout
+        this.setLayout(new GridLayout(4, 4));
 
         for (JButton boton : grupoBotones) {
-                this.add(boton);
+            this.add(boton);
         }
     }
-    
-    public JButton[] getgrupoBotones(){
-            return grupoBotones;
-    }  
-}
 
+    public JButton[] getgrupoBotones() {
+        return grupoBotones;
+    }
+}
