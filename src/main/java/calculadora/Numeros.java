b21b14ed1f160666b76be70a5955fe9d1cf6d378
Moreno27 <alejanmm2000@gmail.com>
2021-04-01 16:38:26 +0200

    Clase definitiva para los numeros(mas los botones de limpiar texto y parentesis(estos ultimos de momento no van a tener ninguna funcion en el programa))

--- src/main/java/calculadora/Numeros.java
+++ src/main/java/calculadora/Numeros.java
@@ -1,22 +1,20 @@
 /* 
- *      
- *      |7| |8| |9|
- *      |4| |5| |6|
- *      |1| |2| |3|
- *      |  0  | |.|
+ *      | | | | |C| |/|
+ *      |7| |8| |9| |*|
+ *      |4| |5| |6| |-|
+ *      |1| |2| |3| |+|
+ *      |  0  | |.| |=|
  *
  *      this.numeros[0][2] = new JButton("9");
- *      int ancho = this.numeros[0][2].getWidth();
  *      this.numeros[0][1] = new JButton("8");
  *      this.numeros[0][0] = new JButton("7");
  *      this.numeros[1][2] = new JButton("6");
  *      this.numeros[1][1] = new JButton("5");
  *      this.numeros[1][0] = new JButton("4");
- *      this.numeros[2][3] = new JButton("3");
- *      this.numeros[2][3] = new JButton("2");
- *      this.numeros[2][3] = new JButton("1");
+ *      this.numeros[2][2] = new JButton("3");
+ *      this.numeros[2][1] = new JButton("2");
+ *      this.numeros[2][0] = new JButton("1");
  *      this.numeros[3][0] = new JButton("0");
- *      this.numeros[3][0].setSize(ancho * 2, HEIGHT);
  *      this.numeros[3][1] = new JButton(".");
  */
 package calculadora;
@@ -42,14 +40,18 @@ public class Numeros extends JPanel {
 
     private void initComponents() {
 
-        numeros = new JButton[4][3];
+        numeros = new JButton[5][3];
 
+        this.numeros[0][0] = new JButton("(");
+        this.numeros[0][1] = new JButton(")");
+        this.numeros[0][2] = new JButton("C");
+        
         /**
          * En este for lo que ahcemos es rellenar de la forma mas eficiente
          * todos los numeros del 1 al 9 como en una calculadora real
          */
         int numero = 9;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 1; i < 5; i++) {
             for (int j = 2; j >= 0; j--) {
                 numeros[i][j] = new JButton(String.valueOf(numero));
                 numero--;
@@ -61,20 +63,22 @@ public class Numeros extends JPanel {
          * del todo de la pagina, necesitaremos, primero crear los botones que
          * queremos y luego redimensionar el boton que tenra el 0
          */
-        this.numeros[3][0] = new JButton("0");
-        this.numeros[3][1] = new JButton(".");
-        this.numeros[3][2] = new JButton("");
+        
+        this.numeros[4][0] = new JButton("0");
+        this.numeros[4][1] = new JButton(".");
+        this.numeros[4][2] = new JButton("");
 
-        this.setLayout(new GridLayout(4, 3));
+        this.setLayout(new GridLayout(5, 3));
 
         //vamos a añadir cada boton
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 3; j++) {
 
                 this.add(numeros[i][j]);
             }
         }
 
+        //no realiza nada
         int ancho = this.numeros[3][0].getWidth() * 2;
         int alto = this.numeros[3][0].getHeight();
         this.numeros[3][0].setSize(new Dimension(alto, ancho));
@@ -83,4 +87,15 @@ public class Numeros extends JPanel {
     public JButton[][] getNumeros() {
         return numeros;
     }
+    
+//    public static void main(String[] args) {
+//
+//        JFrame frame = new JFrame("Calculadora");
+//        frame.setResizable(false);
+//        frame.setLocationRelativeTo(null);
+//        frame.add(new Numeros());
+//        frame.pack();
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    }
 }
