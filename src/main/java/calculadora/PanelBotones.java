f5794c255efcf1d168f5925c32d28fee8b78ad87
Moreno27 <alejanmm2000@gmail.com>
2021-04-04 13:00:09 +0200

    Una vez creados los numeros y botones de operaciones, juntamos ambas clases en una

---
+++ src/main/java/calculadora/PanelBotones.java
@@ -0,0 +1,55 @@
+/*
+
+        grupoBotones[10] = new JButton("+");
+        grupoBotones[11] = new JButton("-");
+        grupoBotones[12] = new JButton("*");
+        grupoBotones[13] = new JButton("/");
+        grupoBotones[14] = new JButton("=");
+        grupoBotones[15] = new JButton("C");
+ */
+package calculadora;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author aleja
+ */
+public class PanelBotones extends JPanel {
+
+    private Numeros numeros;
+    private Botonera simbolos;
+
+    public PanelBotones() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        this.add(new Numeros());
+        this.add(new Botonera());
+        
+    }
+
+    public Numeros getNumeros() {
+        return numeros;
+    }
+
+    public Botonera getSimbolos() {
+        return simbolos;
+    }
+    
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculadora");
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(new PanelBotones());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
