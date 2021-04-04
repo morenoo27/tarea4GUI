f3df367ee26dc8de981246c0ec9cf0ca5221c582
Moreno27 <alejanmm2000@gmail.com>
2021-04-04 20:57:13 +0200

    fallo de tontos, no inicializo lsa cosas(1000 iq)

--- src/main/java/calculadora/Pantalla.java
+++ src/main/java/calculadora/Pantalla.java
@@ -15,44 +15,41 @@ import java.awt.Font;
  *
  * @author Alejandro Moreno Martín 1ºDAW
  */
-public class Pantalla extends JTextArea { //extends JPanel
+public class Pantalla extends JPanel { //extends JPanel
 
-//    private TextArea operacion;
-//    private TextArea resultado;
-//
-//    private final int CARACTERES_MAXIMOS = 19;
-//
-//    public Pantalla() {
-//
-//        initComponents();
-//    }
-//
-//    private void initComponents() {
-//
-//        //establecemos que ambas areas no se pueden editar
-//        this.operacion.setEditable(false);
-//        this.resultado.setEditable(false);
-//
-//        this.operacion.setRows(1);
-//        this.operacion.setColumns(CARACTERES_MAXIMOS);
-//
-//        this.resultado.setRows(1);
-//        this.resultado.setColumns(CARACTERES_MAXIMOS);
-//
-//        this.setLayout(new BorderLayout());
-//        this.add(operacion, BorderLayout.NORTH);
-//        this.add(resultado, BorderLayout.SOUTH);
-//    }
-    
-    private final int FONT_SIZE = 18;
-    private final int LIMITE_ESCRITURA = 10;
-    
-    public final int CARACTERES_MAXIMOS = 19;
+    private TextArea operacion;
+    private TextArea resultado;
+
+    private final int CARACTERES_MAXIMOS = 19;
 
     public Pantalla() {
-        this.setEditable(false);
-        this.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
-        this.setRows(1);
-        this.setColumns(CARACTERES_MAXIMOS);
+
+        initComponents();
     }
+
+    private void initComponents() {
+
+        this.operacion = new TextArea(1, CARACTERES_MAXIMOS);
+        this.resultado = new TextArea(1, CARACTERES_MAXIMOS);
+        
+        //establecemos que ambas areas no se pueden editar
+        this.operacion.setEditable(false);
+        this.resultado.setEditable(false);
+
+        this.setLayout(new BorderLayout());
+        this.add(operacion, BorderLayout.NORTH);
+        this.add(resultado, BorderLayout.SOUTH);
+    }
+    
+//    private final int FONT_SIZE = 18;
+//    private final int LIMITE_ESCRITURA = 10;
+//    
+//    public final int CARACTERES_MAXIMOS = 19;
+//
+//    public Pantalla() {
+//        this.setEditable(false);
+//        this.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
+//        this.setRows(1);
+//        this.setColumns(CARACTERES_MAXIMOS);
+//    }
 }
