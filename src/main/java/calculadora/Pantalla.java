d2443ac8ed36c771c11c19f9872a9a77ebbadfc2
Moreno27 <alejanmm2000@gmail.com>
2021-04-04 16:20:10 +0200

    De momento, para continuar con el proyecto, he copiado codigo de Alberto Lopez

--- src/main/java/calculadora/Pantalla.java
+++ src/main/java/calculadora/Pantalla.java
@@ -6,41 +6,53 @@
 package calculadora;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.TextArea;
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.Font;
 
 /**
  *
- * @author aleja
+ * @author Alejandro Moreno Martín 1ºDAW
  */
-public class Pantalla extends JTextArea {
+public class Pantalla extends JTextArea { //extends JPanel
 
-    private TextArea operacion;
-    private TextArea resultado;
-
-    private final int CARACTERES_MAXIMOS = 19;
+//    private TextArea operacion;
+//    private TextArea resultado;
+//
+//    private final int CARACTERES_MAXIMOS = 19;
+//
+//    public Pantalla() {
+//
+//        initComponents();
+//    }
+//
+//    private void initComponents() {
+//
+//        //establecemos que ambas areas no se pueden editar
+//        this.operacion.setEditable(false);
+//        this.resultado.setEditable(false);
+//
+//        this.operacion.setRows(1);
+//        this.operacion.setColumns(CARACTERES_MAXIMOS);
+//
+//        this.resultado.setRows(1);
+//        this.resultado.setColumns(CARACTERES_MAXIMOS);
+//
+//        this.setLayout(new BorderLayout());
+//        this.add(operacion, BorderLayout.NORTH);
+//        this.add(resultado, BorderLayout.SOUTH);
+//    }
+    
+    private final int FONT_SIZE = 18;
+    private final int LIMITE_ESCRITURA = 10;
+    
+    public final int CARACTERES_MAXIMOS = 19;
 
     public Pantalla() {
-
-        initComponents();
-    }
-
-    private void initComponents() {
-
-        //establecemos que ambas areas no se pueden editar
-        this.operacion.setEditable(false);
-        this.resultado.setEditable(false);
-
-        this.operacion.setRows(1);
-        this.operacion.setColumns(CARACTERES_MAXIMOS);
-
-        this.resultado.setRows(1);
-        this.resultado.setColumns(CARACTERES_MAXIMOS);
-
-        this.setLayout(new BorderLayout());
-        this.add(operacion, BorderLayout.NORTH);
-        this.add(resultado, BorderLayout.SOUTH);
+        this.setEditable(false);
+        this.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
+        this.setRows(1);
+        this.setColumns(CARACTERES_MAXIMOS);
     }
 }
